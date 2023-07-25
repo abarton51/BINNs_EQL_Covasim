93d25acc66ee9a2c34a62fb7c633891aa9460712
Austin Barton <101911867+abarton51@users.noreply.github.com>
2023-07-25 11:07:01 -0400

    Pruning algorithm demo

--- Modules/Utils/PruneEquation.py
+++ Modules/Utils/PruneEquation.py
@@ -24,43 +24,47 @@ def PruneEquation(theta_old_dict, y, alpha, max_pruning=-1) -> dict:
     '''
     theta_new_dict = dict()
     theta_new_dict['old_features'] = theta_old_dict['features']
-    old_feature_arr = theta_new_dict['features']
+    old_feature_arr = theta_new_dict['old_features']
     pruned_features = np.array([])
     
     theta_0 = theta_old_dict['theta']
     lm = LinearRegression()
+    lm.fit(theta_0, y)
+    theta_new_dict['xi'] = lm.coef_.T
     scoring = 'neg_mean_squared_error'
     cv0 = cross_val_score(lm, theta_0, y, cv=5, scoring=scoring)
     val0 = cv0.mean()*-1
     theta_new_dict['val0'] = val0
     
-    def prune(lm, theta_curr, y, val_0, curr_feature_arr, alpha):
+    def prune(lm, theta_curr, y, val_0, curr_feature_arr, pruned_features, alpha):
         '''
         Single pruning pass
         '''
         for i, name in enumerate(curr_feature_arr):
             theta_i = np.delete(theta_0, i, axis=1)
+            lm.fit(theta_i, y)
             cv_i = cross_val_score(lm, theta_i, y, cv=5, scoring='neg_mean_squared_error')
             val_i = cv_i.mean()*-1
             
             if val_i / val0 < 1 + alpha:
                 pruned_features = np.append(pruned_features, name)
-                return True, theta_i, val_i
+                theta_new_dict['xi'] = lm.coef_.T
+                return True, theta_i, val_i, pruned_features
             
             else:
-                return False, theta_curr, val_0
+                return False, theta_curr, val_0, pruned_features
     
     theta_curr = theta_0.copy()
     curr_feature_arr = old_feature_arr.copy()
     pruned = True
     num_pruned = 0
-    while pruned:
-        pruned, theta_curr, val_curr = prune(lm, theta_curr, y, val0, curr_feature_arr, alpha)
-        if not pruned:
+    max_pruning = theta_0.shape[1] if max_pruning == -1 else max_pruning
+    while pruned and num_pruned <= max_pruning:
+        pruned, theta_curr, val_curr, pruned_features = prune(lm, theta_curr, y, val0, curr_feature_arr, pruned_features, alpha)
+        if pruned:
             num_pruned += 1
             
     theta_new_dict['theta'] = theta_curr
-    theta_new_dict['xi'] = lm.coef_
     theta_new_dict['pruned'] = True if num_pruned > 0 else False
     theta_new_dict['features'] = np.array([])
     for elem in old_feature_arr:
