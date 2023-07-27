c78ea4f4c5ae1310b34d22592b3e49ef3c36e7c7
Austin Barton <101911867+abarton51@users.noreply.github.com>
2023-07-27 09:52:51 -0400

    evaluation and update pde solver

--- Modules/Utils/PDESolver.py
+++ Modules/Utils/PDESolver.py
@@ -113,79 +113,21 @@ def chi_func(t, chi_type):
         factor = eff_ub
     return factor
 
-def STEAYDQRF_RHS_dynamic(t, y, contact_rate, quarantine_test, tau_func, params, t_max, chi_type):
-    '''
-    RHS evaluation of learned components for the STEAYDQRF model.
-    
-    Args:
-        t (array): time vector.
-        y (array): vector of values of STEAYDQRF.
-        contact_rate (func): the contact rate learned MLP in the BINN model.
-        quarantine_test (func): the quarantining rate learned MLP in the BINN model.
-        tau_func (func): the quarantine diagnoses rate learned MLP in the BINN model.
-        params (dict): paramters of COVASIM model.
-        t_max (float): the maximum value of time in the t array.
-        chi_type (str): string indicated the type of function chi is.
-    
-    Returns:
-        (array): numpy array of values of each differential term in the ODE system.
-    '''
-
-    population = params['population']
-    alpha = params['alpha']
-    gamma = params['gamma']
-    mu = params['mu']
-    lamda = params['lamda']
-    p_asymp = params['p_asymp']
-    n_contacts = params['n_contacts']
-    delta = params['delta']
-
-    eff_ub = params['eff_ub']
-    chi = chi_func(t, chi_type)
-    # get contact rates from learned MLP
-    array = y[None, :][:, [0, 3, 4]].reshape(1,-1) # , chi
-    cr = contact_rate(array).reshape(-1)
-    yita = params['yita_lb'] + (params['yita_ub'] - params['yita_lb']) * cr[0]
-    yq_array = np.append(y[None, :][:,[0, 3, 4]].sum(axis=1, keepdims=True), chi).reshape(1,-1)
-    beta0 = quarantine_test(yq_array).reshape(-1)
-    beta = chi * beta0
-    
-    ay_array = y[None, :][:, [3, 4]].reshape(1,-1)
-    tau0 = tau_func(ay_array)
-    tau = params['tau_lb'] + (params['tau_ub'] - params['tau_lb']) * tau0
-    # current compartment values
-    s, tq, e, a, y, d, q, r, f = y[0], y[1], y[2], y[3], y[4], y[5], y[6], y[7], y[8]
-    new_d = mu * y +  tau * q
-    # dS
-    ds = - yita * s * (a + y) - beta * new_d *  n_contacts * s + alpha * tq
-
-    # dT
-    dt =  beta * new_d *  n_contacts * s - alpha * tq
-
-    # dE
-    de = yita * s * (a + y) - gamma * e
-
-    # dA
-    da =  p_asymp * gamma * e - lamda * a - beta * new_d *  n_contacts * a
-
-    # dY
-    dy = (1 - p_asymp) * gamma * e - (mu + lamda + delta) * y - beta * new_d *  n_contacts * y
-
-    # dD
-    dd =  mu * y + tau * q - lamda * d - delta * d
-
-    # dQ
-    dq =  beta * new_d *  n_contacts * (a + y) - (tau + delta) * q
-
-    # dR
-    dr =  lamda * (a + y + d ) #
-
-    # dF
-    df =  delta * (y + d + q) #
-
-    return np.array([ds, dt, de, da, dy, dd, dq, dr, df])
-
-def STEAYDQRF_RHS_dynamic(t, y, contact_rate, quarantine_test, tau_func, params, t_max, chi_type, regression=False):
+def STEAYDQRF_RHS_dynamic(t, 
+                          y, 
+                          eta_func, 
+                          beta_func, 
+                          tau_func, 
+                          params, 
+                          t_max, 
+                          chi_type, 
+                          regression=False,
+                          eta_all_comps=False,
+                          beta_all_comps=False,
+                          tau_all_comps=False,
+                          eta_degree=-1,
+                          beta_degree=-1,
+                          tau_degree=-1):
     '''
     RHS evaluation of learned components for the STEAYDQRF model.
     
@@ -219,14 +161,14 @@ def STEAYDQRF_RHS_dynamic(t, y, contact_rate, quarantine_test, tau_func, params,
         eta_input = y[None, :][:, [0, 3, 4]].reshape(1,-1) # , chi
     else:
         eta_input = y[None, :].reshape(1,-1)
-    eta = contact_rate(eta_input).reshape(-1)
+    eta = eta_func(eta_input).reshape(-1)
     yita = params['yita_lb'] + (params['yita_ub'] - params['yita_lb']) * eta[0]
     
     if not regression:
         beta_input = np.append(y[None, :][:,[0, 3, 4]].sum(axis=1, keepdims=True), chi).reshape(1,-1)
     else:
         beta_input = y[None, :].reshape(1, -1)
-    beta0 = quarantine_test(beta_input).reshape(-1)
+    beta0 = beta_func(beta_input).reshape(-1)
     beta = chi * beta0
     
     if not regression:
