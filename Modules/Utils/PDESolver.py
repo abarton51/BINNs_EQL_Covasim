0ba940c12ee130d1406436054d0a8d4213c7337b
Austin Barton <101911867+abarton51@users.noreply.github.com>
2023-07-27 11:26:07 -0400

    Update PDESolver and Evaluation notebook

--- Modules/Utils/PDESolver.py
+++ Modules/Utils/PDESolver.py
@@ -22,69 +22,6 @@ from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 from sklearn.preprocessing import PolynomialFeatures
 
-def STEAYDQRF_RHS(t, y, contact_rate, quarantine_test, params, t_max):
-    '''
-    RHS evaluation of learned components for the STEAYDQRF model.
-    
-    Inputs:
-    t (float): time.
-    y (float): 
-    contact_rate (func): the contact rate learned MLP in the BINN model.
-    quarantine_test (func): the quarantining rate learned MLP in the BINN model.
-    params (dict): parameters of the Covasim model.
-    t_max (float): the maximum value of time in the t array.
-    '''
-
-    population = params['population']
-    alpha = params['alpha']
-    beta = params['beta']
-    gamma = params['gamma']
-    mu = params['mu']
-    # tau = params['tau']
-    lamda = params['lamda']
-    p_asymp = params['p_asymp']
-    n_contacts = params['n_contacts']
-    delta = params['delta']
-
-    # get contact rates from learned MLP
-    cr = contact_rate(y[None, :][:, [0, 3, 4]]).reshape(-1)
-    yita = params['yita_lb'] + (params['yita_ub'] - params['yita_lb']) * cr[0]
-    qt = quarantine_test(y[None, :][:, [3, 4]]).reshape(-1)
-    tau = params['tau_lb'] + (params['tau_ub'] - params['tau_lb']) * qt
-    # current compartment values
-    s, tq, e, a, y, d, q, r, f = y[0], y[1], y[2], y[3], y[4], y[5], y[6], y[7], y[8]
-    new_d = mu * y +  tau * q
-    # dS
-    ds = - yita * s * (a + y) -  beta * new_d *  n_contacts * s +  alpha * tq
-
-    # dT
-    dt =  beta * new_d *  n_contacts * s -  alpha * tq
-
-    # dE
-    de = yita * s * (a + y) -  gamma * e
-
-    # dA
-    da =  p_asymp *  gamma * e -  lamda * a -  beta * new_d *  n_contacts * a
-
-    # dY
-    dy = (1 -  p_asymp) *  gamma * e - ( mu +  lamda +  delta) * y -  beta * new_d *  n_contacts * y
-
-    # dD
-    dd =  new_d -  lamda * d - delta * d
-
-    # dQ
-    dq =  beta * new_d *  n_contacts * (a + y) - (tau +  lamda + delta) * q
-
-    # dR
-    dr =  lamda * (a + y + d + q)
-
-    # dF
-    df =  delta * (y + d + q)
-
-    # print(np.array([ds, dt, de, da, dy, dd, dq, dr, df]).sum())
-
-    return np.array([ds, dt, de, da, dy, dd, dq, dr, df])
-
 def chi_func(t, chi_type):
     eff_ub = 0.3
     if chi_type == 'linear':
@@ -113,117 +50,21 @@ def chi_func(t, chi_type):
         factor = eff_ub
     return factor
 
-def STEAYDQRF_RHS_dynamic(t, 
-                          y, 
-                          eta_func, 
-                          beta_func, 
-                          tau_func, 
-                          params, 
-                          t_max, 
-                          chi_type, 
-                          regression=False,
-                          eta_all_comps=False,
-                          beta_all_comps=False,
-                          tau_all_comps=False,
-                          eta_degree=-1,
-                          beta_degree=-1,
-                          tau_degree=-1):
-    '''
-    RHS evaluation of learned components for the STEAYDQRF model.
-    
-    Args:
-        t (array): time vector.
-        y (array): vector of values of STEAYDQRF.
-        contact_rate (func): the contact rate learned MLP in the BINN model.
-        quarantine_test (func): the quarantining rate learned MLP in the BINN model.
-        tau_func (func): the quarantine diagnoses rate learned MLP in the BINN model.
-        params (dict): paramters of COVASIM model.
-        t_max (float): the maximum value of time in the t array.
-        chi_type (str): string indicated the type of function chi is.
-    
-    Returns:
-        (array): numpy array of values of each differential term in the ODE system.
-    '''
-
-    population = params['population']
-    alpha = params['alpha']
-    gamma = params['gamma']
-    mu = params['mu']
-    lamda = params['lamda']
-    p_asymp = params['p_asymp']
-    n_contacts = params['n_contacts']
-    delta = params['delta']
-
-    eff_ub = params['eff_ub']
-    chi = chi_func(t, chi_type)
-    # get contact rates from learned MLP
-    if not regression:
-        eta_input = y[None, :][:, [0, 3, 4]].reshape(1,-1) # , chi
-    else:
-        eta_input = y[None, :].reshape(1,-1)
-    eta = eta_func(eta_input).reshape(-1)
-    yita = params['yita_lb'] + (params['yita_ub'] - params['yita_lb']) * eta[0]
-    
-    if not regression:
-        beta_input = np.append(y[None, :][:,[0, 3, 4]].sum(axis=1, keepdims=True), chi).reshape(1,-1)
-    else:
-        beta_input = y[None, :].reshape(1, -1)
-    beta0 = beta_func(beta_input).reshape(-1)
-    beta = chi * beta0
-    
-    if not regression:
-        tau_input = y[None, :][:, [3, 4]].reshape(1,-1)
-    else:
-        tau_input = y[None, :].reshape(1, -1)
-    tau0 = tau_func(tau_input)
-    tau = params['tau_lb'] + (params['tau_ub'] - params['tau_lb']) * tau0
-    # current compartment values
-    s, tq, e, a, y, d, q, r, f = y[0], y[1], y[2], y[3], y[4], y[5], y[6], y[7], y[8]
-    new_d = mu * y +  tau * q
-    # dS
-    ds = - yita * s * (a + y) - beta * new_d *  n_contacts * s + alpha * tq
-
-    # dT
-    dt =  beta * new_d *  n_contacts * s - alpha * tq
-
-    # dE
-    de = yita * s * (a + y) - gamma * e
-
-    # dA
-    da =  p_asymp * gamma * e - lamda * a - beta * new_d *  n_contacts * a
-
-    # dY
-    dy = (1 - p_asymp) * gamma * e - (mu + lamda + delta) * y - beta * new_d *  n_contacts * y
-
-    # dD
-    dd =  mu * y + tau * q - lamda * d - delta * d
-
-    # dQ
-    dq =  beta * new_d *  n_contacts * (a + y) - (tau + delta) * q
-
-    # dR
-    dr =  lamda * (a + y + d ) #
-
-    # dF
-    df =  delta * (y + d + q) #
-
-    return np.array([ds, dt, de, da, dy, dd, dq, dr, df])
-
-def STEAYDQRF_RHS_dynamic_maskarr(t, 
-                                  y, 
-                                  eta_func, 
-                                  beta_func, 
-                                  tau_func, 
-                                  params, 
-                                  t_max, 
-                                  chi_type, 
-                                  regression=False,
-                                  eta_all_comps=False,
-                                  beta_all_comps=False,
-                                  tau_all_comps=False,
-                                  eta_degree=-1,
-                                  beta_degree=-1,
-                                  tau_degree=-1):
+def STEAYDQRF_RHS_dynamic_DRUMS(t, 
+                                y, 
+                                eta_func, 
+                                beta_func, 
+                                tau_func, 
+                                params, 
+                                t_max, 
+                                chi_type, 
+                                masking=False,
+                                eta_all_comps=False,
+                                beta_all_comps=False,
+                                tau_all_comps=False,
+                                eta_degree=-1,
+                                beta_degree=-1,
+                                tau_degree=-1):
     '''
     RHS evaluation of learned components for the STEAYDQRF model.
     
@@ -253,27 +94,13 @@ def STEAYDQRF_RHS_dynamic_maskarr(t,
     eff_ub = params['eff_ub']
     
     chi = chi_func(t, chi_type)
-    # get contact rates from learned MLP
     # eta
-    '''
-    if not regression:
-        eta_input = y[None, :][:, [0, 3, 4]].reshape(1,-1)
-        if int(t * t_max) < 183:
-            eta_input = np.append(eta_input, avg_masking[int(t * t_max)]).reshape(1, -1)
-        else:
-            eta_input = np.append(eta_input, avg_masking[-1]).reshape(1, -1)
-    else:
-        eta_input = y[None, :].reshape(1,-1)
-        if int(t * t_max) < 183:
-            eta_input = np.append(eta_input, avg_masking[int(t * t_max)]).reshape(1, -1)
-        else:
-            eta_input = np.append(eta_input, avg_masking[-1]).reshape(1, -1)
-    '''
     eta_input = y[None, :][:, [0, 3, 4]].reshape(1,-1) if not eta_all_comps else y[None, :].reshape(1,-1)
-    if int(t * t_max) < 183:
-        eta_input = np.append(eta_input, avg_masking[int(t * t_max)]).reshape(1, -1)
-    else:
-        eta_input = np.append(eta_input, avg_masking[-1]).reshape(1, -1)
+    if masking:
+        if int(t * t_max) < 183:
+            eta_input = np.append(eta_input, avg_masking[int(t * t_max)]).reshape(1, -1)
+        else:
+            eta_input = np.append(eta_input, avg_masking[-1]).reshape(1, -1)
     if eta_degree==-1:
         cr = eta_func(eta_input).reshape(-1)
     else:
@@ -283,12 +110,6 @@ def STEAYDQRF_RHS_dynamic_maskarr(t,
     yita = params['yita_lb'] + (params['yita_ub'] - params['yita_lb']) * cr[0]
     
     # beta
-    '''
-    if not regression:
-        beta_input = np.append(y[None, :][:,[0, 3, 4]].sum(axis=1, keepdims=True), chi).reshape(1,-1)
-    else:
-        beta_input = y[None, :].reshape(1, -1)
-    '''
     if not beta_all_comps:
         beta_input = np.append(y[None, :][:,[0, 3, 4]].sum(axis=1, keepdims=True), chi).reshape(1,-1)
     else:
@@ -302,12 +123,6 @@ def STEAYDQRF_RHS_dynamic_maskarr(t,
     beta = chi * beta0
     
     # tau
-    '''
-    if not regression:
-        tau_input = y[None, :][:, [3, 4]].reshape(1,-1)
-    else:
-        tau_input = y[None, :].reshape(1, -1)
-    '''
     tau_input = y[None, :][:, [3, 4]].reshape(1,-1) if not tau_all_comps else y[None, :].reshape(1,-1)
     if tau_degree==-1:
         tau0 = tau_func(tau_input)
@@ -357,7 +172,7 @@ def STEAYDQRF_sim(RHS,
                   tau_func, 
                   params, 
                   chi_type, 
-                  regression=False,
+                  masking=False,
                   eta_all_comps=False,
                   beta_all_comps=False,
                   tau_all_comps=False,
@@ -402,7 +217,7 @@ def STEAYDQRF_sim(RHS,
                    params, 
                    t_max, 
                    chi_type, 
-                   regression,
+                   masking,
                    eta_all_comps,
                    beta_all_comps,
                    tau_all_comps, 
